58518a1450b1f797e5dc1f751a1a9a552ff32a09
Harsh <harshbhama96@gmail.com>
2019-03-26 10:47:24 +0530

    initial-todolist

---
+++ src/Todolist.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Todolist(){
+  return(
+        <div className = "todoitem">
+          <input type = "checkbox" />
+          <p className = "p-style"> Text </p>
+        </div>
+
+
+  )
+}
+
+export default Todolist
