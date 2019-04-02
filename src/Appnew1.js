f2a246d9172f024a0bce9d78a159f06f254bbc60
Harsh <harshbhama96@gmail.com>
2019-04-02 21:30:58 +0530

    state practice-1

--- src/Appnew1.js
+++ src/Appnew1.js
@@ -1,24 +1,42 @@
 import React from "react"
 
-class Appnew1 extends React.Component{
+// class Appnew1 extends React.Component{
+//
+//   constructor(){
+//     super()
+//     this.state = {
+//       name: "harsh",
+//       age: "22"
+//     }
+//   }
+//
+//
+//   render(){
+//     return(
+//       <div>
+//         <h1>{this.state.name}</h1>
+//         <h3>{this.state.age}</h3>
+//       </div>
+//     )
+//   }
+// }
 
-  constructor(){
-    super()
-    this.state = {
-      name: "harsh",
-      age: "22"
+  class Appnew1 extends React.Component{
+
+    constructor(){
+      super()
+      this.isloggedin = {
+        value: false
+      }
+    }
+
+    render(){
+      return(
+        <h1> You are currently logged {this.isloggedin.value?"in":"out"} </h1>
+      )
     }
   }
 
 
-  render(){
-    return(
-      <div>
-        <h1>{this.state.name}</h1>
-        <h3>{this.state.age}</h3>
-      </div>
-    )
-  }
-}
 
 export default Appnew1
